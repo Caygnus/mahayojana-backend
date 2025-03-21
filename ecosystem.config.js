1bc16c69fffe8cc7d090021003da0a3f1f41d8df
omkar sonawane <omkarsonawane622@gmail.com>
2025-03-22 01:40:49 +0530

    Update ecosystem configuration, deployment workflow, and Swagger documentation
    - Changed the PM2 script in ecosystem.config.js to point to the new server entry file.
    - Updated the deploy.yml workflow to ensure environment variables are loaded from a .env file before restarting the backend.
    - Enhanced Swagger documentation by uncommenting issuer and audience properties in tokenInfo for better clarity and completeness.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,14 @@ module.exports = {
   apps: [
     {
       name: 'mahayojana-backend',
-      script: 'npm',
-      args: 'run start',
+      script: 'build/server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
       env: {
-        ...process.env,
-        PORT: 8081,
         NODE_ENV: 'production',
-      },
-    },
-  ],
+        DB_NAME: 'mahayojana-dev-db'
+      }
+    }
+  ]
 };
