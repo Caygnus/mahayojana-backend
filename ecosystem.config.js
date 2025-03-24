43b0b0a91a6462dbf9364fd6aa053de599f29c41
omkar sonawane <omkarsonawane622@gmail.com>
2025-03-25 04:42:40 +0530

    Update deployment configuration for environment variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,9 +28,8 @@ module.exports = {
       repo: "https://github.com/Caygnus/mahayojana-backend",
       path: "/home/deploy/mahayojana-backend",
       ssh_options: "StrictHostKeyChecking=no",
-      "pre-deploy-local": "npm run env:encrypt && echo 'Encrypting environment variables...'",
-      "pre-deploy": "npm run env:decrypt",
-      "post-deploy": "npm install && npm run build && pm2 restart ecosystem.config.js --env production",
+      "pre-deploy-local": "npm run env:encrypt && scp .env.store deploy@178.16.137.230:/home/deploy/mahayojana-backend/current/.env.store",
+      "post-deploy": "npm install && npm install -g dotenv-store && DOTENV_STORE_PASSPHRASE=caygnus dotenv-store decrypt && npm run build && pm2 restart ecosystem.config.js --env production",
       "pre-setup": "echo 'Setting up server...'",
       key: "~/.ssh/caygnus"
     }
