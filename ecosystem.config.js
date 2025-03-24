863363bd0f86c4c2296cc7cbc2081e859d46bc76
omkar sonawane <omkarsonawane622@gmail.com>
2025-03-25 04:42:54 +0530

    Update ecosystem configuration, enhance deployment scripts, and modify package scripts
    - Updated the ecosystem.config.js to support cluster mode and added deployment configurations for production.
    - Modified the package.json scripts to include new commands for PM2 deployment and management.
    - Adjusted environment variables for development and production settings in the ecosystem configuration.
    - Commented out the DailyRotateFile import in Logger.ts for potential future use.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,35 +3,37 @@ require('dotenv').config(); // Load .env file
 module.exports = {
   apps: [
     {
-      name: "mahayojana-backend",
-      script: "build/server.js",
-      exec_mode: "cluster",
-      instances: "max",
+      name: 'mahayojana-backend',
+      script: 'build/server.js',
+      exec_mode: 'cluster',
+      instances: 'max',
       autorestart: true,
       watch: false,
-      max_memory_restart: "1G",
+      max_memory_restart: '1G',
       env: {
-        NODE_ENV: "development",
-        PORT: 3000
+        NODE_ENV: 'development',
+        PORT: 3000,
       },
       env_production: {
-        NODE_ENV: "production",
-        PORT: 8080
-      }
-    }
+        NODE_ENV: 'production',
+        PORT: 8080,
+      },
+    },
   ],
   deploy: {
     production: {
-      user: "deploy",
-      host: "178.16.137.230",
-      ref: "origin/main",
-      repo: "https://github.com/Caygnus/mahayojana-backend",
-      path: "/home/deploy/mahayojana-backend",
-      ssh_options: "StrictHostKeyChecking=no",
-      "pre-deploy-local": "npm run env:encrypt && scp .env.store deploy@178.16.137.230:/home/deploy/mahayojana-backend/current/.env.store",
-      "post-deploy": "npm install && npm install -g dotenv-store && DOTENV_STORE_PASSPHRASE=caygnus dotenv-store decrypt && npm run build && pm2 restart ecosystem.config.js --env production",
-      "pre-setup": "echo 'Setting up server...'",
-      key: "~/.ssh/caygnus"
-    }
-  }
+      user: 'deploy',
+      host: '178.16.137.230',
+      ref: 'origin/main',
+      repo: 'https://github.com/Caygnus/mahayojana-backend',
+      path: '/home/deploy/mahayojana-backend',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'pre-deploy-local':
+        'npm run env:encrypt && scp .env.store deploy@178.16.137.230:/home/deploy/mahayojana-backend/current/.env.store',
+      'post-deploy':
+        'npm install && npm install -g dotenv-store && DOTENV_STORE_PASSPHRASE=caygnus dotenv-store decrypt && npm run build && pm2 restart ecosystem.config.js --env production',
+      'pre-setup': "echo 'Setting up server...'",
+      key: '~/.ssh/caygnus',
+    },
+  },
 };
